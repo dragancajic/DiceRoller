e366a4696962ec0f8681d59e120e357289598e89
Dragan Cajic <cajic_dragan@yahoo.com>
2021-10-28 00:58:25 +0200

    ~ Use Random Number Generator
    > java.util.Random public Random()
    > change the text in the TextView.

--- app/src/main/java/org/eu5/learn_pisio/diceroller/MainActivity.kt
+++ app/src/main/java/org/eu5/learn_pisio/diceroller/MainActivity.kt
@@ -3,8 +3,10 @@ package org.eu5.learn_pisio.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
+//import android.widget.Toast
 import kotlinx.android.synthetic.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 	
@@ -18,7 +20,32 @@ class MainActivity : AppCompatActivity() {
 		// This setter takes a single Lambda function --> setOnClickListener { }
 		// which is triggered every time that button is clicked --> roll_button.
 		rollButton.setOnClickListener {
-			Toast.makeText(this, "button clicked!", Toast.LENGTH_SHORT).show()
+			// test that the button click works!
+			//Toast.makeText(this, "button clicked!", Toast.LENGTH_SHORT).show()
+			
+			// method to be called when the button is clicked
+			rollDice()
 		}
+	}
+	
+	// implement rollDice() method
+	private fun rollDice() {
+		// TODO("Not yet implemented")
+		// generate random number from semi-interval/semi-segment [0, 6)
+		// invoking constructor Random() of standard Java class (java.util.Random)
+		val randomInt = Random().nextInt(6) + 1 // <-- java.util.Random public Random()
+		
+		// get a reference to the TextView result_text
+		val resultText: TextView = findViewById(R.id.result_text)
+		
+		// test button click to TextView result_text
+		//resultText.text = "Dice Rolled!"
+		
+		// set generated number as text of TextView result_text:
+		// a) using setter method <-- from Java!
+		//resultText.setText(randomInt.toString())
+		// or
+		// b) property access syntax <-- Kotlin!
+		resultText.text = randomInt.toString()
 	}
 }
