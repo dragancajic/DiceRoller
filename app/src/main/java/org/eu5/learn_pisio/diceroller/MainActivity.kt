6e9deaa5376777725f2070ed0282eee8e09ac350
Dragan Cajic <cajic_dragan@yahoo.com>
2021-10-27 23:43:03 +0200

    ~ Test Button Click
    > set the OnClickListener for the button using a single Lambda,
    > make a Toast (message) to be shown when the button is clicked!

--- app/src/main/java/org/eu5/learn_pisio/diceroller/MainActivity.kt
+++ app/src/main/java/org/eu5/learn_pisio/diceroller/MainActivity.kt
@@ -2,11 +2,23 @@ package org.eu5.learn_pisio.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.*
 
 class MainActivity : AppCompatActivity() {
 	
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+		
+		val rollButton: Button = findViewById(R.id.roll_button)
+		rollButton.text = getString(R.string.roll_button)
+		
+		// This setter takes a single Lambda function --> setOnClickListener { }
+		// which is triggered every time that button is clicked --> roll_button.
+		rollButton.setOnClickListener {
+			Toast.makeText(this, "button clicked!", Toast.LENGTH_SHORT).show()
+		}
 	}
 }
