13bd5c47de0b0cf645a3eca0e3289a67157cedde
Dragan Cajic <cajic_dragan@yahoo.com>
2021-11-02 23:59:28 +0100

    ~ Add Dice Image(s)

--- app/src/main/java/org/eu5/learn_pisio/diceroller/MainActivity.kt
+++ app/src/main/java/org/eu5/learn_pisio/diceroller/MainActivity.kt
@@ -3,9 +3,10 @@ package org.eu5.learn_pisio.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 //import android.widget.Toast
-import kotlinx.android.synthetic.*
+
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -37,6 +38,22 @@ class MainActivity : AppCompatActivity() {
 		
 		// get a reference to the TextView result_text
 		val resultText: TextView = findViewById(R.id.result_text)
+		
+		// get a reference to the ImageView dice_image
+		val diceImage: ImageView = findViewById(R.id.dice_image)
+		
+		// choose the right drawable resource based on the value of randomInt
+		val drawableResource = when (randomInt) {
+			1 -> R.drawable.dice_1
+			2 -> R.drawable.dice_2
+			3 -> R.drawable.dice_3
+			4 -> R.drawable.dice_4
+			5 -> R.drawable.dice_5
+			else -> R.drawable.dice_6
+		}
+		
+		// assign the drawableResource to diceImage
+		diceImage.setImageResource(drawableResource)
 		
 		// test button click to TextView result_text
 		//resultText.text = "Dice Rolled!"
